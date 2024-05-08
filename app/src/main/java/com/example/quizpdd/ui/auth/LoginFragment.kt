b77be84ac5f81cfd8ffb4a71fd881662df1382d4
vladbstrv <keocer@gmail.com>
2024-05-08 23:18:49 +0300

    ui login

---
+++ app/src/main/java/com/example/quizpdd/ui/auth/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.example.quizpdd.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.quizpdd.R
+import com.example.quizpdd.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_topicFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
